03a71da28a63d088f1624f5288c1af7124b3743f
User <bz300788@gmail.com>
2024-02-13 08:08:19 +0300

    attestation

--- cook_project/settings.py
+++ cook_project/settings.py
@@ -91,15 +91,15 @@ WSGI_APPLICATION = 'cook_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'cookingbook$cookingbook',
-        'USER': 'cookingbook',
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'cookingbook.mysql.pythonanywhere-services.com',
-        'OPTIONS': {'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
-                    'charset': 'utf8mb4',
-                    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': 'cookingbook$default',
+        # 'USER': 'cookingbook',
+        # 'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        # 'HOST': 'cookingbook.mysql.pythonanywhere-services.com',
+        # 'OPTIONS': {'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+        #             'charset': 'utf8mb4',
+        #             },
     }
 }
 
